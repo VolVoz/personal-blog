064efffb61acddd01e20d3f5ad023655d05301dd
Volodymyr Vozniak <vozniak.vol@hotmail.com>
2016-03-26 23:14:29 +0200

    move import

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request
 from datetime import datetime
-import models
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
+
+import models
 
 
 @app.route('/')
