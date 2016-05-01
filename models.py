af1ea6b42c30d9c25bb0fa7762d49eb2b2beecbd
Volodymyr Vozniak <vozniak.vol@hotmail.com>
2016-05-01 13:33:55 +0300

    update db

--- models.py
+++ models.py
@@ -7,18 +7,6 @@ from micawber import parse_html
 from flask import Markup
 
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, email):
-        self.email = email
-
-    def __repr__(self):
-        return '<E-mail %r>' % self.email
-
-
 class Entry(db.Model):
     __tablename__ = "entries"
     id = db.Column(db.Integer, primary_key=True)
