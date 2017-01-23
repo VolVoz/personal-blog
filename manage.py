f1c21dbb8cbfa5576ba1bd1a3ec5b787c5771f4e
Volodymyr Vozniak <vozniak.vol@hotmail.com>
2017-01-23 15:42:52 +0000

    run tests from manage script

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 import os
+import unittest
+import coverage
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
@@ -13,6 +15,40 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 manager.add_command('start', Server(host=os.environ.get('IP', '0.0.0.0'),  # command for run app in browser IDE
                                     port=int(os.environ.get('PORT', 8080))))
+
+@manager.command
+def test():
+    """Runs the unit tests without coverage."""
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    cov = coverage.coverage(
+        branch=True,
+        include='project/*',
+        omit=['*/__init__.py']
+    )
+    cov.start()
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    cov.stop()
+    cov.save()
+    print 'Coverage Summary:'
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'tmp/coverage')
+    cov.html_report(directory=covdir)
+    print('HTML version: file://%s/index.html' % covdir)
+    cov.erase()
+
+
+@manager.command
+def create_data():
+    """Creates sample data."""
+    pass
 
 if __name__ == '__main__':
     manager.run()
